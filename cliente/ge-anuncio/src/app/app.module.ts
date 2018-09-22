30a4732b4b60bdd65409f8c8a35c5ab423d42c86
hugoiguana <hugo.iguanaa@gmail.com>
2018-09-22 20:15:23 -0300

    [def] Corrigindo caminho do compomente ad.

--- cliente/ge-anuncio/src/app/app.module.ts
+++ cliente/ge-anuncio/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AdModule } from '../ad/ad.module';
+import { AdModule } from './ad/ad.module';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 
