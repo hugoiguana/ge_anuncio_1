390694c288517eecdbc214df9ae805f44bbfd2a7
hugoiguana <hugo.iguanaa@gmail.com>
2018-09-29 12:14:31 -0300

    Configuração inicial do projeto geanuncio1(Servidor Springboot)

---
+++ servidor/springboot/geanuncio1/src/main/java/com/hugoiguana/br/geanuncio1/Geanuncio1Application.java
@@ -0,0 +1,12 @@
+package com.hugoiguana.br.geanuncio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Geanuncio1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Geanuncio1Application.class, args);
+    }
+}
